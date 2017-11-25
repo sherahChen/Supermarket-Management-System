39ef74aac3741ce3a7f8a22d861aef3e1c12a542
Leon <490868429@qq.com>
2017-11-25 11:27:59 +0800

    为mongodb.js增加了增删查改方法,兼容性不强,待完善

--- src/api/db/mongodb.js
+++ src/api/db/mongodb.js
@@ -5,8 +5,8 @@ var connstr = 'mongodb://127.0.0.1:27017/retails';
 var apiresult = require('../modules/apiresult');
 
 var db;
-client.connect(connstr, function(_error, _db){
-    if(_error){
+client.connect(connstr, function (_error, _db) {
+    if (_error) {
         console.log(_error);
     } else {
         db = _db;
@@ -14,16 +14,31 @@ client.connect(connstr, function(_error, _db){
 })
 
 module.exports = {
-    select: function(_collection, _condition, _cb){
-        db.collection(_collection).find(_condition || {}).toArray(function(error, result){
+    select: function (_collection, _condition, _cb) {
+        db.collection(_collection).find(_condition || {}).toArray(function (error, result) {
             _cb(apiresult(error ? false : true, error || result));
         })
     },
-    insert : function(_collection, _data, _cb){
-        db.collection(_collection).insert(_data, function(error, result){
+    insert: function (_collection, _data, _cb) {
+        db.collection(_collection).insert(_data, function (error, result) {
             _cb(apiresult(error ? false : true, error || result));
         })
     },
-    update: function(){},
-    delete: function(){}
+    update: function (_collection, _data, _cb) {
+        db.collection(_collection).update({
+            name: _data.name
+        }, {
+            $set: {
+                name: _data.rename
+            }
+        }).then(function (result) {
+
+            _cb(apiResult(true));
+        });
+    },
+    delete: function (_collection, _data, _cb) {
+        db.collection(_collection).remove(_data).then(function (result) {
+            _cb(apiresult(true));
+        });
+    }
 }
